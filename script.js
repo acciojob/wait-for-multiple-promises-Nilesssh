b4a7b0bd17759f570c8ed36730df710a018c974f
Nilesssh <103926034+Nilesssh@users.noreply.github.com>
2023-05-01 11:52:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
 //your JS code here. If required.
+function createPromise() {
+  return new Promise((resolve, reject) => {
+    const timeout = Math.floor(Math.random() * 3) + 1;
+    setTimeout(() => {
+      resolve(timeout);
+    }, timeout * 1000);
+  });
+}
+
+const promises = [createPromise(), createPromise(), createPromise()];
+
+const table = document.getElementById("myTable");
+const loadingRow = table.insertRow();
+const loadingCell = loadingRow.insertCell();
+loadingCell.colSpan = 2;
+loadingCell.innerHTML = "Loading...";
+
+Promise.all(promises)
+  .then((results) => {
+    table.deleteRow(0);
+
+    results.forEach((result, index) => {
+      const row = table.insertRow();
+      const promiseCell = row.insertCell();
+      promiseCell.innerHTML = `Promise ${index + 1}`;
+      const timeCell = row.insertCell();
+      timeCell.innerHTML = `${result} s`;
+    });
+
+    const totalRow = table.insertRow();
+    const totalCell = totalRow.insertCell();
+    totalCell.innerHTML = "Total";
+    const totalTimeCell = totalRow.insertCell();
+    const totalTime = results.reduce((acc, curr) => acc + curr, 0);
+    totalTimeCell.innerHTML = `${totalTime.toFixed(3)} s`;
+  })
+  .catch((error) => {
+    console.log(error);
+  });
